a29e7744a963655dee8df077361b0b58e51f0270
xahid71 <jahid.info97@gmail.com>
2023-09-19 10:03:22 +0600

    update : login swagger done

--- features/login/login_routes.js
+++ features/login/login_routes.js
@@ -1,3 +1,81 @@
+/**
+ * @swagger
+ * /api/log-in:
+ *   post:
+ *     summary: Login to the Snapped URL system
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fcm_token:
+ *                 type: string
+ *                 description: Firebase Cloud Messaging token (optional)
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: User's email
+ *               password:
+ *                 type: string
+ *                 description: User's password
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: JWT token for authentication
+ *                 user_info:
+ *                   type: object
+ *                   properties:
+ *                     user_id:
+ *                       type: number
+ *                       description: User's unique ID
+ *                     email:
+ *                       type: string
+ *                       format: email
+ *                       description: User's email address
+ *       400:
+ *         description: Invalid request data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Error message
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Unauthorized error message
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Internal server error message
+ */
+
+
 const express = require('express');
 const router = express.Router();
 const loginController = require('./login_controller');
